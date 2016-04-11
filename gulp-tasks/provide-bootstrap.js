ff123f9f2363e7ca880cbbac2f8abc121feec99d
Axel Duch <axlduch@gmail.com>
2016-04-12 00:43:20 +0200

    Update gulp task to build bootstrap to static dir

--- gulp-tasks/provide-bootstrap.js
+++ gulp-tasks/provide-bootstrap.js
@@ -4,11 +4,29 @@ var less = require('gulp-less');
 var plumber = require('gulp-plumber');
 
 gulp.task('provide-bootstrap', function () {
-    var bootstrapPath = [
-        __dirname + '/../node_modules/bootstrap-material-design/dist/bootstrap-material-design.min.css',
-        __dirname + '/../node_modules/bootstrap-material-design/dist/bootstrap-material-design.min.css.map',
+    var materialPath = __dirname + '/../node_modules/bootstrap-material-design/dist/';
+    var bootstrapPath = __dirname + '/../node_modules/bootstrap/dist/';
+
+    var cssPaths = [
+        bootstrapPath + 'css/bootstrap.min.css',
+        materialPath + 'css/bootstrap-material-design.min.css',
+        materialPath + 'css/bootstrap-material-design.min.css.map',
+        materialPath + 'css/ripple.min.css',
+        materialPath + 'css/ripple.min.css.map'
     ];
 
-    return gulp.src(bootstrapPath)
+    var jsPaths = [
+        __dirname + '/../node_modules/jquery/dist/jquery.min.js',
+        bootstrapPath + 'js/bootstrap.min.js',
+        materialPath + 'js/ripples.min.js',
+        materialPath + 'js/ripples.min.js.map',
+        materialPath + 'js/material.min.js',
+        materialPath + 'js/material.js.map'
+    ];
+
+    gulp.src(cssPaths)
         .pipe(gulp.dest(__dirname + '/../static/css'));
+
+    gulp.src(jsPaths)
+        .pipe(gulp.dest(__dirname + '/../static/ext_lib'));
 });
