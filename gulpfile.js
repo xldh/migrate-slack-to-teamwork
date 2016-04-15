c7180ad68955572f1da9b57a8274cf8a164aef7a
Axel Duch <axlduch@gmail.com>
2016-04-15 04:50:37 +0200

    Fix gulp tasks path usage

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp');
 var registerTasks = require('./utils/gulp').registerTasks;
-registerTasks(__dirname + '/gulp-tasks');
+registerTasks(__dirname + '/gulp_tasks');
 
 gulp.task('default', ['provide-bootstrap', 'watch-webpack', 'watch-less']);
